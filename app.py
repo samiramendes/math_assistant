45b3269aedc40cf9d459713023ccad4111b2a5c9
Samira Mendes <samiramendes@alumni.usp.br>
2025-08-24 11:15:04 -0300

    tools setup

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 import chainlit as cl
+from langchain_groq import ChatGroq
+from langchain.agents.agent_types import AgentType
+from langchain.agents import Tool, initialize_agent
 from dotenv import load_dotenv
 import os
-
+from tools.calculator import calculator
+from tools.web_searcher import wikipedia_search
 
 load_dotenv()
 llm_api_key = os.getenv("GROQ_API_KEY")
+
+    
+@cl.on_chat_start
+def math_assistent():
+    # Create Groq LLM 
+    llm = ChatGroq(model="moonshotai/kimi-k2-instruct", temperature=0.0)
+
+    agent = initialize_agent(
+        tools=[calculator, wikipedia_search],
+        llm=llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=False,
+        handle_parsing_errors=True
+    )
+    cl.user_session.set("agent", agent)
 
 @cl.on_message
 async def process_message(message: cl.Message):
--- tools/calculator.py
+++ tools/calculator.py
@@ -2,22 +2,36 @@ import math
 import numexpr
 from langchain.tools import tool
 
-@tool
+@tool("calculator-tool")
 def calculator(expression: str) -> str:
     """Calculate expression using Python's numexpr library.
 
-    Expression should be a single line mathematical expression
-    that solves the problem.
+    The expression must be a valid mathematical string (e.g., "128 * 46", "2**3 + 5", "pi * 2**2").
 
     Examples:
         "37593 * 67" for "37593 times 67"
         "37593**(1/5)" for "37593^(1/5)"
     """
-    local_dict = {"pi": math.pi, "e": math.e}
-    return str(
-        numexpr.evaluate(
-            expression.strip(),
-            global_dict={},  # restrict access to globals
-            local_dict=local_dict,  # add common mathematical functions
-        )
-    )
+    try: 
+        local_dict = {
+            "pi": math.pi,
+            "e": math.e,
+            "sqrt": math.sqrt,
+            "log": math.log,
+            "log10": math.log10,
+            "sin": math.sin,
+            "cos": math.cos,
+            "tan": math.tan,
+            "abs": abs,
+            "pow": pow
+        }
+        result = numexpr.evaluate(
+                expression.strip(),
+                global_dict={},  # restrict access to globals
+                local_dict=local_dict,  # add common mathematical functions
+            )
+        return str(result)
+    
+    except Exception as e:
+        return (f"Failed to calculate the expression. Make sure it's a valid math expression using numbers and operators only (Error: {str(e)})"
+)
